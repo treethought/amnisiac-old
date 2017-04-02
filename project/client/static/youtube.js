4c9f4dab496fa794d15d62e68b842457be124a2a
Cam Sweeney <cpsweene@gmail.com>
2017-04-02 04:14:08 -0700

    render single iframe, change between posts

--- project/client/static/youtube.js
+++ project/client/static/youtube.js
@@ -1,221 +1,131 @@
-// # js for creating youtube play buttons from invisible video player
-// $( document ).ready(function() {
 
 
-
+var player = null;
 
 function onYouTubeIframeAPIReady() {
 
+
+    console.log(player)
+
     console.log('In youtubeready');
 
-    var playerItems = document.getElementsByClassName('youtube-item');
+    var postItems = document.getElementsByClassName('post-item');
+    console.log('Number of posts: ' + postItems.length)
 
-    Array.from(playerItems).forEach( function(elem, i) {
-        // statements
-   
-    // for (var i = 0; i < playerItems.length; i++) {
-        // var elem = playerItems[i];
-        console.log('Working on ' + i)
+    Array.from(postItems).forEach(function(elem, i) {
 
+        if (i === 0) {}
+
+        // Render Icon
         var icon = document.createElement("img");
-        icon.setAttribute("id", "youtube-icon-" + i);
-        icon.setAttribute("src", "https://i.imgur.com/" + 'IDzX9gL.png');
+        icon.setAttribute("id", "post-icon-" + i);
+        icon.setAttribute("class", 'icon');
+        icon.setAttribute("src", "https://i.imgur.com/" + "quyUPXN.png");
 
         icon.style.cssText = "cursor:pointer;cursor:hand";
         icon.style.height = '50px';
         icon.style.width = '50px';
         icon.setAttribute('class', "img-responsive")
         elem.appendChild(icon);
-        console.log(elem.getElementsByTagName('img'));
 
-
-        var div = document.createElement("div");
-        div.setAttribute("id", "youtube-player-"+ i);
-        elem.appendChild(div);
-        console.log('Added player div to the element');
-
-        var divImg = elem.getElementsByTagName('img')
 
         var toggleButon = function(e) {
-        var iconSource = e ? "IDzX9gL.png" : "quyUPXN.png";
+            var iconSource = e ? "IDzX9gL.png" : "quyUPXN.png";
 
-        icon.setAttribute("src", "https://i.imgur.com/" + iconSource);
+            icon.setAttribute("src", "https://i.imgur.com/" + iconSource);
         };
-        console.log('Added iconSource to player');
 
-        var setNowPlayingImg = function (videoId) {
-            var playingImg = document.getElementById('now-playing-thumb');
-            var playingInfo = document.getElementById('now-playing-info');
-            console.log('Setting image for ' + videoId);
-            var imgUrl = "http://img.youtube.com/vi/"+videoId+"/0.jpg";
-            var title = document.getElementById('post-title-'+videoId).innerHTML;
-            playingImg.setAttribute('src', imgUrl);
-            playingInfo.innerHTML = title;
-
-
-
-
-
+        var startNext = function () {
+            document.getElementById('post-icon-'+(i+1)).click();
 
         }
 
-
-        elem.onclick = function() {
-        if (player.getPlayerState() === YT.PlayerState.PLAYING
-            || player.getPlayerState() === YT.PlayerState.BUFFERING) {
-
-            player.pauseVideo();
-            toggleButon(false);
-            
-
-        }
-        else {
-            player.playVideo();
-            toggleButon(true);
-            setNowPlayingImg(elem.dataset.video)
-
-        }
-    };
-
-        // var play_next = function () {
-        //     var nextId = 'youtube-player-' + (i+1);
-        //     console.log('trying to play ' + nextId)
-        //     // callPlayer(nextId, 'PlayVideo');
-
-        //     var nextElem = document.getElementById(nextId);
-        //     nextElem.style.background = 'red';
+        elem.addEventListener('click', function() {
+            console.log('clicked ' + elem.id);
+            // console.log('State was '+ player.getPlayerState());
 
 
-        //     var nextPlayer = new YT.Player(nextId, {});
-        //     nextPlayer.playVideo();
-        // };
+            if (!YT.get('youtube-player')) {
+                renderPlayer(elem.id);
+                // player = YT.get('youtube-player');
+                console.log('CREATED FIRST PLAYER with:' + elem.id);
+                // console.log('PLAYER VAR:')
+            }
 
-        
-            
+            if (player.a.src.toString().includes(elem.id)) {
+                console.log(elem.id+ ' is ' + player.getPlayerState());
 
-        var player = new YT.Player("youtube-player-"+ i, {
-            height: "0",
-            width: "0",
-            videoId: elem.dataset.video,
-            playerVars: {
-                autoplay: elem.dataset.autoplay,
-                loop: elem.dataset.loop
-            },
-            events: {
-                onReady: function(e) {
-                    e.target.setPlaybackQuality("small");
-                    toggleButon(e.target.getPlayerState() !== YT.PlayerState.CUED);
+                if (player.getPlayerState() === YT.PlayerState.PLAYING
+                    || player.getPlayerState() === YT.PlayerState.BUFFERING) {
+
+
+                    console.log('pausing video ' + elem.id)
+                        // console.log(player.)
+                    player.pauseVideo();
+                    toggleButon(false);
+                }
+
+                if (player.getPlayerState() === YT.PlayerState.CUED) {
+                
+                    // console.log(elem.id + ' is cued');
+                    player.playVideo();
+                    toggleButon(true);
+                }
+
+
+            } else { // start new vid
+                console.log('starting new video ' + elem.id)
+                player.destroy();
+                toggleButon(false)
+                renderPlayer(elem.id);
+
+                player.playVideo()
+                toggleButon(true)
+
+            }
+
+
+
+
+        })
+
+
+
+
+        function renderPlayer(postVideoID) {
+  
+            console.log('rendering for ' + postVideoID);
+
+            player = new YT.Player("youtube-player", {
+                // // height: "100",
+                // width: "0",
+                videoId: postVideoID,
+                playerVars: {
+                    autoplay: true,
+                    loop: false,
+                    color: 'purple',
+                    controls: 2,
+                    enablejsapi: 1
                 },
-                onStateChange: function(e) {
-                    if (e.data === YT.PlayerState.ENDED) {
-                        toggleButon(false);
+                events: {
+                    onReady: function(e) {
+                        e.target.setPlaybackQuality("medium");
+                        toggleButon(e.target.getPlayerState() !== YT.PlayerState.CUED);
 
-
-
-                      }
-
-                  }
-              }
-          });
-
-
-    
-
-     });
-
-}
-
-
-
-
-
-/**
- * @author       Rob W <gwnRob@gmail.com>
- * @website      http://stackoverflow.com/a/7513356/938089
- * @version      20131010
- * @description  Executes function on a framed YouTube video (see website link)
- *               For a full list of possible functions, see:
- *               https://developers.google.com/youtube/js_api_reference
- * @param String frame_id The id of (the div containing) the frame
- * @param String func     Desired function to call, eg. "playVideo"
- *        (Function)      Function to call when the player is ready.
- * @param Array  args     (optional) List of arguments to pass to function func*/
-function callPlayer(frame_id, func, args) {
-    console.log('calling player')
-    if (window.jQuery && frame_id instanceof jQuery) frame_id = frame_id.get(0).id;
-    var iframe = document.getElementById(frame_id);
-    if (iframe && iframe.tagName.toUpperCase() != 'IFRAME') {
-        iframe = iframe.getElementsByTagName('iframe')[0];
-    }
-
-    // When the player is not ready yet, add the event to a queue
-    // Each frame_id is associated with an own queue.
-    // Each queue has three possible states:
-    //  undefined = uninitialised / array = queue / 0 = ready
-    if (!callPlayer.queue) callPlayer.queue = {};
-    var queue = callPlayer.queue[frame_id],
-        domReady = document.readyState == 'complete';
-
-    if (domReady && !iframe) {
-        // DOM is ready and iframe does not exist. Log a message
-        window.console && console.log('callPlayer: Frame not found; id=' + frame_id);
-        if (queue) clearInterval(queue.poller);
-    } else if (func === 'listening') {
-        // Sending the "listener" message to the frame, to request status updates
-        if (iframe && iframe.contentWindow) {
-            func = '{"event":"listening","id":' + JSON.stringify(''+frame_id) + '}';
-            iframe.contentWindow.postMessage(func, '*');
-        }
-    } else if (!domReady ||
-               iframe && (!iframe.contentWindow || queue && !queue.ready) ||
-               (!queue || !queue.ready) && typeof func === 'function') {
-        if (!queue) queue = callPlayer.queue[frame_id] = [];
-        queue.push([func, args]);
-        if (!('poller' in queue)) {
-            // keep polling until the document and frame is ready
-            queue.poller = setInterval(function() {
-                callPlayer(frame_id, 'listening');
-            }, 250);
-            // Add a global "message" event listener, to catch status updates:
-            messageEvent(1, function runOnceReady(e) {
-                if (!iframe) {
-                    iframe = document.getElementById(frame_id);
-                    if (!iframe) return;
-                    if (iframe.tagName.toUpperCase() != 'IFRAME') {
-                        iframe = iframe.getElementsByTagName('iframe')[0];
-                        if (!iframe) return;
+                    },
+                    onStateChange: function(e) {
+                        if (e.data === YT.PlayerState.ENDED) {
+                            toggleButon(false);
+                            startNext();
+                            // start next post
+                        }
                     }
                 }
-                if (e.source === iframe.contentWindow) {
-                    // Assume that the player is ready if we receive a
-                    // message from the iframe
-                    clearInterval(queue.poller);
-                    queue.ready = true;
-                    messageEvent(0, runOnceReady);
-                    // .. and release the queue:
-                    while (tmp = queue.shift()) {
-                        callPlayer(frame_id, tmp[0], tmp[1]);
-                    }
-                }
-            }, false);
-        }
-    } else if (iframe && iframe.contentWindow) {
-        // When a function is supplied, just call it (like "onYouTubePlayerReady")
-        if (func.call) return func();
-        // Frame exists, send message
-        iframe.contentWindow.postMessage(JSON.stringify({
-            "event": "command",
-            "func": func,
-            "args": args || [],
-            "id": frame_id
-        }), "*");
-    }
-    /* IE8 does not support addEventListener... */
-    function messageEvent(add, listener) {
-        var w3 = add ? window.addEventListener : window.removeEventListener;
-        w3 ?
-            w3('message', listener, !1)
-        :
-            (add ? window.attachEvent : window.detachEvent)('onmessage', listener);
-    }
-}
+            });
+            return player
+
+
+
+
+        };
+
