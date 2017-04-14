e1e699ab85cd82a63f608b11fbd7c222baf4a585
Cam Sweeney <cpsweene@gmail.com>
2017-04-13 23:41:26 -0700

    seperated forms

---
+++ project/server/reddit/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import Form
+from wtforms import TextField, SelectMultipleField, widgets
+from wtforms.validators import Optional
+
+
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    widget = widgets.TableWidget()
+    # widget = MyTableWidget()
+    option_widget = widgets.CheckboxInput()
+
+
+class SourcesForm(Form):
+    """Used in sources.html - autocompletes using select2 and he multicheckbox"""
+
+    follow_sources = MultiCheckboxField('Subs', id='select-field', validators=[Optional()])
+
+    search_bar = TextField('Search Subreddits', id='search-bar')
