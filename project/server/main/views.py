26bb38644259882e30d0bd910c677aec82187323
Cam Sweeney <cpsweene@gmail.com>
2017-06-15 17:28:42 -0700

    move sc_autocomplete to main views, cleaner api imports

--- project/server/main/views.py
+++ project/server/main/views.py
@@ -6,13 +6,13 @@
 #################
 import itertools
 
-from flask import render_template, Blueprint, request, session, redirect, url_for
+from flask import render_template, Blueprint, request, session, redirect, url_for, jsonify
 from flask_login import current_user
 
 
 from project.server.main.forms import HomeSearchForm
-from project.server.reddit.api import build_sources, fetch_submissions
-from project.server.sc.api import fetch_tracks
+from project.server.reddit import api as reddit
+from project.server.sc import api as sc
 
 ################
 #### config ####
@@ -35,9 +35,21 @@ def landing():
 @main_blueprint.route('/home', methods=['GET', 'POST'])
 def home():
     search_form = HomeSearchForm()
-    search_form.follow_sources.choices = build_sources()
+    search_form.follow_sources.choices = reddit.build_sources()
     search_form.process()
     return render_template('main/home.html', form=search_form)
+
+
+# TODO: remove from sc.views and use select2
+@main_blueprint.route('/sc_autocomplete', methods=['GET'])
+def autocomplete():
+    results = []
+    term = request.args.get('q')
+    for user in sc.search_user(term):
+        if term.lower() in user.username.lower():
+            results.append(user.username)
+
+    return jsonify(results=results)
 
 
 @main_blueprint.route('/results', methods=['POST'])
@@ -47,10 +59,10 @@ def results():
 
     if form.validate_on_submit():
         subreddits = form.reddit_search.data.split(',')
-        reddit_posts = fetch_submissions(subreddits)
+        reddit_posts = reddit.fetch_submissions(subreddits)
 
         sc_artists = form.sc_search.data.split(',')
-        sc_tracks = fetch_tracks(sc_artists)
+        sc_tracks = sc.fetch_tracks(sc_artists)
 
         for i in list(itertools.zip_longest(reddit_posts, sc_tracks)):
             items.extend(i)
