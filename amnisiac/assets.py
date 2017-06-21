4ede3aa518e8b448f853a73c82db0488125309b7
Cam Sweeney <cpsweene@gmail.com>
2017-06-20 20:25:42 -0700

    flask-assets, one import of js_common in _base.html

--- amnisiac/assets.py
+++ amnisiac/assets.py
@@ -2,42 +2,63 @@
 """Application assets."""
 from flask_assets import Bundle, Environment
 
-css = Bundle(
-    'libs/bootstrap/dist/css/bootstrap.css',
+css_all = Bundle(
+    'libs/bootstrap/dist/css/bootstrap.min.css',
+    'libs/jquery-ui/themes/smoothness/jquery-ui.css',
+    'libs/select2/dist/css/select2.min.css',
     'main.css',
     filters='cssmin',
     output='public/css/common.css'
 )
 
-# js = Bundle(
-#     'libs/jquery/dist/jquery.js',
-#     'libs/bootstrap/dist/js/bootstrap.js',
-#     'js/plugins.js',
-#     filters='jsmin',
-#     output='public/js/common.js'
-# )
-
-js_search = Bundle(
-    'libs/jquery/dist/jquery.js',
-    'libs/jquery-ui/jquery-ui.js',
+js_common = Bundle(
+    'libs/jquery/dist/jquery.min.js',
+    'libs/jquery-ui/jquery-ui.min.js',
+    'libs/bootstrap/dist/js/bootstrap.min.js',
     "libs/select2/dist/js/select2.full.js",
     'search.js',
-    filters='jsmin',
-    output='public/js/save.js')
-
-js_feed = Bundle(
-    'libs/jquery/dist/jquery.js',
-    'libs/bootstrap/dist/js/bootstrap.js',
     'libs/youtube-iframe-api/youtube.iframe-api.js',
     'libs/api/index.js',
     'feed.js',
     'save.js',
     filters='jsmin',
-    output='public/js/feed.js')
+    output='public/js/common.js'
+)
+
+
+
+# js_all = Bundle(
+#     'libs/jquery/dist/jquery.min.js',
+#     'libs/jquery-ui/jquery-ui.min.js',
+#     'libs/bootstrap/dist/js/bootstrap.min.js',
+#     filters='jsmin',
+#     output='public/js/main.js')
+
+
+# js_search = Bundle(
+#     'libs/jquery/dist/jquery.min.js',
+#     'libs/jquery-ui/jquery-ui.min.js',
+#     'libs/bootstrap/dist/js/bootstrap.min.js',
+#     "libs/select2/dist/js/select2.full.js",
+#     'search.js',
+#     filters='jsmin',
+#     output='public/js/save.js')
+
+# js_feed = Bundle(
+#     'libs/jquery/dist/jquery.min.js',
+#     'libs/jquery-ui/jquery-ui.min.js',
+#     'libs/bootstrap/dist/js/bootstrap.min.js',
+#     'libs/youtube-iframe-api/youtube.iframe-api.js',
+#     'libs/api/index.js',
+#     'feed.js',
+#     'save.js',
+#     filters='jsmin',
+#     output='public/js/feed.js')
 
 assets = Environment()
 
-# assets.register('js_all', js)
-assets.register('css_all', css)
-assets.register('js_feed', js_feed)
-assets.register('js_search', js_search)
+assets.register('js_common', js_common)
+assets.register('css_all', css_all)
+# assets.register('js_all', js_all)
+# assets.register('js_feed', js_feed)
+# assets.register('js_search', js_search)
